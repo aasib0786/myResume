28eb87663330105c7ad76bb622f228815fbea4a3
AASIB078 <aasibkhan0080@gmail.com>
2025-07-31 19:53:02 +0530

    FUXE c

--- src/app/services/fetchServer.js
+++ src/app/services/fetchServer.js
@@ -1,30 +1,31 @@
 // services/apiService.js
 import axios from "axios";
 
-const serverURL = "http://localhost:3000";
+// const serverURL = "http://localhost:3000";
+const serverURL = "https://my-resume-git-main-aasibs-projects.vercel.app";
 
 const postData = async (url, body) => {
-  try {
-    const response = await axios.post(`${serverURL}/api/${url}`, body, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return response.data;
-  } catch (e) {
-    console.error("POST error:", e);
-    return null;
-  }
+    try {
+        const response = await axios.post(`${serverURL}/api/${url}`, body, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return response.data;
+    } catch (e) {
+        console.error("POST error:", e);
+        return null;
+    }
 };
 
 const getData = async (url) => {
-  try {
-    const response = await axios.get(`${serverURL}/api/${url}`);
-    return response.data;
-  } catch (e) {
-    console.error("GET error:", e);
-    return null;
-  }
+    try {
+        const response = await axios.get(`${serverURL}/api/${url}`);
+        return response.data;
+    } catch (e) {
+        console.error("GET error:", e);
+        return null;
+    }
 };
 
 export { serverURL, postData, getData };
